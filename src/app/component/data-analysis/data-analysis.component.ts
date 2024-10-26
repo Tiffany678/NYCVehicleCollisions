4cd58e2b619c1208e1d30b0ace4faa71fc2fdb2b
YINGJIE YANG <yingjieyang@yingjies-mbp.mynetworksettings.com>
2024-10-25 23:07:48 -0400

    update

--- src/app/component/data-analysis/data-analysis.component.ts
+++ src/app/component/data-analysis/data-analysis.component.ts
@@ -1,10 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data-analysis',
   templateUrl: './data-analysis.component.html',
-  styleUrl: './data-analysis.component.css'
+  styleUrl: './data-analysis.component.css',
 })
 export class DataAnalysisComponent {
+  analysis: any = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchProjects().subscribe(
+      (analysis: any[]) => {
+        console.log(analysis);
+        this.analysis = analysis;
+      },
+      (error: any) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
+
+  fetchProjects(): Observable<any[]> {
+    return this.http.get<any[]>('assets/data.json');
+  }
 }
